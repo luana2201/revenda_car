c0ef37558eae64764e5d9362c406ae892cf85d84
luana2201 <souza.siqueira.luana@escola.pr.gov.br>
2026-04-22 11:18:51 -0300

    salvar

--- revenda-carros-mvc/src/config/db.js
+++ revenda-carros-mvc/src/config/db.js
@@ -1,4 +1,13 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-const pool = mysql.createPool({});
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimi: 10,
+    queueLimit: 0
+});
